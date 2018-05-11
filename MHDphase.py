a4ff861dd57cf13dcb659e83bb7efc9329ee0972
tsubok1969 <kent1@mac.com>
2018-05-11 17:20:03 +0900

    Rankine-Hugoniot solver

--- MHDphase.py
+++ MHDphase.py
@@ -1,22 +1,25 @@
+import sys
 import numpy as np
 
 class MHDphase:
     def __init__(self, beta, theta, xacc=1.e-6):
-        # mf: fast Mach number, ms: slow Mach number, mi: another branch of Alfven Mach number
-        # normalized by normal Alfven velocity 
+        # beta and theta : upstream quantities
+        # squared mf: fast MS, ms: slow MS, mi: another branch of Alfven velocity
+        # normalized by the upstream normal Alfven velocity 
         self.beta = beta
         self.theta = theta
-        self.mf = np.sqrt(self.rhsol(1.0+xacc, 1.e+10))
-        self.ms = np.sqrt(self.rhsol(0.0, 1.0-xacc))
-        self.mi = np.sqrt(self.rhsol(0.0, 1.0, alf_mode=True))
+        self.mf = self.rhsol(1.0+xacc, 1.e+10)
+        self.ms = self.rhsol(0.0, 1.0-xacc)
+        self.mi = self.rhsol(0.0, 1.0, alf_mode=True)
+        self.mcd, self.mcu = self.critical_Alfven()
 
-    def ax1(self, ax2):
+    def ax1(self, ax2, gamma=5./3.):
         # determine the square of the upstream Alfven Mach number from that in the downstream
         beta = self.beta
         theta = self.theta
-        tanth2 = np.tan(theta*np.pi/180.)**2
-        costh2 = np.cos(theta*np.pi/180.)**2
-        gamma = 5./3.
+        deg2rad = np.pi/180.
+        tanth2 = np.tan(theta*deg2rad)**2
+        costh2 = np.cos(theta*deg2rad)**2
 
         x1 = ax2*( (gamma-1.)/gamma * ( (gamma+1.)/(gamma-1.) - tanth2 ) * \
                        (ax2-1.)**2 + tanth2*( (gamma-1.)/gamma*ax2 - 1. ) * \
@@ -25,13 +28,12 @@ class MHDphase:
 
         return x1/x2
 
-    def rhsol(self, x1, x2, alf_mode=False, xacc=1.e-6, iteration=100):
+    def rhsol(self, x1, x2, alf_mode=False, xacc=1.e-6, iteration=1000):
         beta = self.beta
         theta = self.theta
         for j in range(iteration):
             if(alf_mode):
-                x1ref = 1.0
-                x2ref = 1.0
+                x1ref = x2ref = 1.0
             else:
                 x1ref = x1
                 x2ref = x2
@@ -55,12 +57,46 @@ class MHDphase:
         print('lack of iteration')
 
     def critical_Alfven(self, step = 1.e-5):
+        # determine the critical Alfven Mach number
+        # Ma_down > xmax: weak intermediate shock
+        # Ma_down < xmax: strong intermediate shock
+        # ymax: critical Alfven Mach number of upstream flow
         ymax = -1.e+10
-        x = self.ms
+        x = (self.ms)**2
         while x < 1.0:
             y = self.ax1(x)
             if y > ymax:
                 xmax = x
                 ymax = y
             x = x + step
-        return xmax
+        return xmax, ymax
+
+    def solsearch(self, xdown, mode):
+        # mode 1: fast shock 2: slow shock 3: IS
+        ax2 = xdown**2
+        deg2rad = np.pi/180.
+        while(True):
+            if mode == 1:
+                if ax2 > self.mf:
+                    break
+            elif mode == 2:
+                if self.ms < ax2 < self.mi:
+                    break
+            else:
+                if self.mi < ax2 < 1.0:
+                    break
+            print('value is not suitable for RH')
+            sys.exit()
+
+        ax1 = self.ax1(ax2)
+        r = ax1/ax2
+        thetad = np.rad2deg( np.arctan( (ax1-1.)/(ax2-1.)*np.tan(self.theta*deg2rad) ) )
+        bd = np.cos(self.theta*deg2rad)/np.cos(thetad*deg2rad)
+        bt = bd * np.sin(thetad*deg2rad)
+        uin = (1.-1./r)*np.sqrt(ax1)*np.cos(self.theta*deg2rad)
+        vshock = np.sqrt(ax2/r)*np.cos(self.theta*deg2rad)
+        print('compressional ratio    : ' + str(r) )
+        print('downstream field angle : ' + str(thetad) )
+        print('downstream field mag.  : ' + str(bd) )
+        print('injection velocity     : ' + str(uin) )
+        print('shock velocity         : ' + str(vshock) )
